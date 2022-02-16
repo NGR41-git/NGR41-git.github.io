e2f598c8a250885d544347993792ec5574386945
Rouhi <80149230+NGR41-git@users.noreply.github.com>
2022-02-16 11:53:54 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta name="keywords" content="tabriz,iran">
-    <meta encoding="utf-8">
+    
     <title>Rouhi</title>
   </head>
 
   <body>
-  <?php echo "In The Name of The Most High"; ?>
-  <p></p>
+  
     
   </body>
 </html>
